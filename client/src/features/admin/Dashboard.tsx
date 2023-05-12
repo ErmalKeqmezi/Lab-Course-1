5e081cf47d5476b561b89fb3091615be51115bf5
ArtinTopojani <artintopojani2002@gmail.com>
2023-05-12 03:05:50 +0200

    Dashboardi

--- client/src/features/admin/Dashboard.tsx
+++ client/src/features/admin/Dashboard.tsx
@@ -1,4 +1,15 @@
-import { Typography, Button, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Box } from "@mui/material";
+import {
+  Typography,
+  Button,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Box,
+} from "@mui/material";
 import { Edit, Delete } from "@mui/icons-material";
 import { currencyFormat } from "../../app/util/util";
 import UseProducts from "../../app/hooks/useProducts";
@@ -13,93 +24,119 @@ import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
 
 export default function Dashboard() {
-    const { products, metaData, productsLoaded } = UseProducts();
-    const dispatch = useAppDispatch();
-    const [editMode, setEditMode] = useState(false);
-    const [selectedProduct, setSelectedProduct] = useState<Product | undefined>(undefined);
-    const [loading, setLoading] = useState(false);
-    const [target, setTarget] = useState(0);
+  const { products, metaData, productsLoaded } = UseProducts();
+  const dispatch = useAppDispatch();
+  const [editMode, setEditMode] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | undefined>(
+    undefined
+  );
+  const [loading, setLoading] = useState(false);
+  const [target, setTarget] = useState(0);
 
-    function handleSelectProduct(product: Product) {
-        setSelectedProduct(product);
-        setEditMode(true);
-    }
+  function handleSelectProduct(product: Product) {
+    setSelectedProduct(product);
+    setEditMode(true);
+  }
 
-    function handleDeleteProduct(id: number) {
-        setLoading(true)
-        setTarget(id);
-        agent.Admin.deleteProduct(id)
-            .then(() => dispatch(deleteProduct(id)))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false))
-    }
+  function handleDeleteProduct(id: number) {
+    setLoading(true);
+    setTarget(id);
+    agent.Admin.deleteProduct(id)
+      .then(() => dispatch(deleteProduct(id)))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
 
-    function cancelEdit() {
-        if(selectedProduct) setSelectedProduct(undefined);
-        setEditMode(false)
-    }
+  function cancelEdit() {
+    if (selectedProduct) setSelectedProduct(undefined);
+    setEditMode(false);
+  }
 
-    if(editMode) return <ProductForm product={selectedProduct} cancelEdit={cancelEdit}/>
-     
+  if (editMode)
+    return <ProductForm product={selectedProduct} cancelEdit={cancelEdit} />;
 
-    if(!productsLoaded) return <LoadingComponent message='Loading products...' />
-    return (
-        <>
-            <Box display='flex' justifyContent='space-between'>
-                <Typography sx={{ p: 2 }} variant='h4'>Inventory</Typography>
-                <Button onClick={() => setEditMode(true)} sx={{ m: 2 }} size='large' variant='contained'>Create</Button>
-            </Box>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>#</TableCell>
-                            <TableCell align="left">Product</TableCell>
-                            <TableCell align="right">Price</TableCell>
-                            <TableCell align="center">Type</TableCell>
-                            <TableCell align="center">Brand</TableCell>
-                            <TableCell align="center">Quantity</TableCell>
-                            <TableCell align="right"></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {products.map((product) => (
-                            <TableRow
-                                key={product.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {product.id}
-                                </TableCell>
-                                <TableCell align="left">
-                                    <Box display='flex' alignItems='center'>
-                                        <img src={product.pictureUrl} alt={product.name} style={{ height: 50, marginRight: 20 }} />
-                                        <span>{product.name}</span>
-                                    </Box>
-                                </TableCell>
-                                <TableCell align="right">{currencyFormat(product.price)}</TableCell>
-                                <TableCell align="center">{product.type}</TableCell>
-                                <TableCell align="center">{product.brand}</TableCell>
-                                <TableCell align="center">{product.quantityInStock}</TableCell>
-                                <TableCell align="right">
-                                    <Button onClick={() => handleSelectProduct(product)} startIcon={<Edit />} />
-                                    <LoadingButton 
-                                    loading={loading && target === product.id} 
-                                    startIcon={<Delete />} 
-                                    color='error' 
-                                    onClick={() => handleDeleteProduct(product.id)}
-                                    />
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            {metaData && 
-                <Box sx={{pt: 2}}> 
-                    <AppPagination  metaData={metaData} onPageChange={(page: number) => dispatch(setPageNumber({pageNumber: page}))} />
-                </Box>
+  if (!productsLoaded)
+    return <LoadingComponent message="Loading products..." />;
+  return (
+    <>
+      <Box display="flex" justifyContent="space-between">
+        <Typography sx={{ p: 2 }} variant="h4">
+          Inventory
+        </Typography>
+        <Button
+          onClick={() => setEditMode(true)}
+          sx={{ m: 2 }}
+          size="large"
+          variant="contained"
+        >
+          Create
+        </Button>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell align="left">Product</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="center">Type</TableCell>
+              <TableCell align="center">Brand</TableCell>
+              <TableCell align="center">Quantity</TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow
+                key={product.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {product.id}
+                </TableCell>
+                <TableCell align="left">
+                  <Box display="flex" alignItems="center">
+                    <img
+                      src={product.pictureUrl}
+                      alt={product.name}
+                      style={{ height: 50, marginRight: 20 }}
+                    />
+                    <span>{product.name}</span>
+                  </Box>
+                </TableCell>
+                <TableCell align="right">
+                  {currencyFormat(product.price)}
+                </TableCell>
+                <TableCell align="center">{product.type}</TableCell>
+                <TableCell align="center">{product.brand}</TableCell>
+                <TableCell align="center">{product.quantityInStock}</TableCell>
+                <TableCell align="right">
+                  <Button
+                    onClick={() => handleSelectProduct(product)}
+                    startIcon={<Edit />}
+                  />
+                  <LoadingButton
+                    loading={loading && target === product.id}
+                    startIcon={<Delete />}
+                    color="error"
+                    onClick={() => handleDeleteProduct(product.id)}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {metaData && (
+        <Box sx={{ pt: 2 }}>
+          <AppPagination
+            metaData={metaData}
+            onPageChange={(page: number) =>
+              dispatch(setPageNumber({ pageNumber: page }))
             }
-        </>
-    )
+          />
+        </Box>
+      )}
+    </>
+  );
 }
